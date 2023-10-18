4ee922f3af15f895e8867d3eca9718885c5a708c
João Vitor S <jvsg27@hotmail.com>
2023-10-18 07:53:53 -0300

    V3

--- src/javascript/user.js
+++ src/javascript/user.js
@@ -2,21 +2,30 @@ document.addEventListener('DOMContentLoaded', function () {
     const loggedInUser = localStorage.getItem('loggedInUser');
     const taskForm = document.getElementById('task-form');
     const taskList = document.getElementById('task-list');
+    const taskListComplete = document.getElementById('task-list-complete');
     const logoutButton = document.getElementById('logout-button');
+    const clearCompletedButton = document.getElementById('clear-completed-button');
 
     if (!loggedInUser) {
-        alert('Você precisa fazer login para acessar esta área!');
-        window.location.href = 'login.html';
+        alert('Você precisa fazer login para acessar esta área.');
+        window.location.href = './login.html';
     }
 
-    // Adição de um evento 'clique' no botão de logout
+    // Adiciona o evento de clique ao botão de logout
     logoutButton.addEventListener('click', function () {
         localStorage.removeItem('loggedInUser');
-        window.location.href = '../../index.html'; 
-        // Redirecione para a página inicial (index.html)
+        window.location.href = '../../index.html';
     });
 
-    // Adição de um evento 'clique' no botão de editar tarefa
+    // Carrega as tarefas concluídas no histórico
+    loadCompletedTasks();
+
+    // Adiciona o evento de clique ao botão "Limpar Concluídas"
+    clearCompletedButton.addEventListener('click', function () {
+        clearCompletedTasks();
+    });
+
+    // Adiciona o evento de clique ao botão de editar tarefa
     taskList.addEventListener('click', function (event) {
         if (event.target.tagName === 'BUTTON' && event.target.classList.contains('edit-button')) {
             const taskId = event.target.getAttribute('data-id');
@@ -24,7 +33,78 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    // Adição de um evento 'clique' no botão de salvar edição de tarefa
+    // Adiciona o evento de clique ao botão de completar tarefa
+    taskList.addEventListener('click', function (event) {
+        if (event.target.tagName === 'BUTTON' && event.target.classList.contains('complete-button')) {
+            const taskId = event.target.getAttribute('data-id');
+            completeTask(taskId);
+        }
+    });
+
+    // Função para limpar as tarefas concluídas
+    function clearCompletedTasks() {
+        // Limpa as tarefas concluídas do histórico
+        localStorage.removeItem(loggedInUser + "_completed");
+        loadCompletedTasks(); // Atualiza a lista de tarefas concluídas (que agora estará vazia)
+    }
+
+    // Função para marcar uma tarefa como concluída
+    function completeTask(taskId) {
+        const userTasks = JSON.parse(localStorage.getItem(loggedInUser)) || [];
+        const completedTask = userTasks.splice(taskId, 1)[0];
+        completedTask.state = "Concluída";
+
+        const completedTasksHistory = JSON.parse(localStorage.getItem(loggedInUser + "_completed")) || [];
+        completedTasksHistory.push(completedTask);
+        localStorage.setItem(loggedInUser + "_completed", JSON.stringify(completedTasksHistory));
+
+        localStorage.setItem(loggedInUser, JSON.stringify(userTasks));
+        loadTasks();
+        loadCompletedTasks(); // Atualiza a lista de tarefas concluídas
+    }
+
+    function loadCompletedTasks() {
+        const completedTasksHistory = JSON.parse(localStorage.getItem(loggedInUser + "_completed")) || [];
+
+        taskListComplete.innerHTML = ''; // Limpa a lista de tarefas concluídas
+
+        completedTasksHistory.forEach((task, index) => {
+            const listItem = document.createElement('li');
+            listItem.innerHTML = `
+                <strong>Nome:</strong> ${task.name}<br>
+                <strong>Descrição:</strong> ${task.description}<br>
+                <strong>Prazo:</strong> ${task.dueDate}<br>
+                <strong>Estado:</strong> ${task.state}<br>
+                <button data-id="${index}" class="undo-complete-button">Desfazer Conclusão</button>
+                <div class="espace"></div>
+            `;
+            taskListComplete.appendChild(listItem);
+
+            // Adiciona o evento de clique ao botão "Desfazer Conclusão"
+            const undoCompleteButton = listItem.querySelector('.undo-complete-button');
+            undoCompleteButton.addEventListener('click', function () {
+                undoCompleteTask(index);
+            });
+        });
+    }
+
+    // Função para desfazer a conclusão de uma tarefa
+    function undoCompleteTask(taskId) {
+        const completedTasksHistory = JSON.parse(localStorage.getItem(loggedInUser + "_completed")) || [];
+        const undoneTask = completedTasksHistory.splice(taskId, 1)[0];
+        undoneTask.state = "Pendente";
+
+        // Salva a tarefa desfeita de volta na lista de tarefas pendentes
+        const userTasks = JSON.parse(localStorage.getItem(loggedInUser)) || [];
+        userTasks.push(undoneTask);
+        localStorage.setItem(loggedInUser + "_completed", JSON.stringify(completedTasksHistory));
+        localStorage.setItem(loggedInUser, JSON.stringify(userTasks));
+
+        loadCompletedTasks(); // Atualiza a lista de tarefas concluídas
+        loadTasks(); // Atualiza a lista de tarefas pendentes
+    }
+
+    // Adiciona o evento de clique ao botão de salvar edição de tarefa
     taskList.addEventListener('click', function (event) {
         if (event.target.tagName === 'BUTTON' && event.target.classList.contains('save-button')) {
             const taskId = event.target.getAttribute('data-id');
@@ -32,7 +112,14 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    // Adicione um evento de clique ao botão de excluir tarefa
+    // Adiciona o evento de clique ao botão de cancelar edição de tarefa
+    taskList.addEventListener('click', function (event) {
+        if (event.target.tagName === 'BUTTON' && event.target.classList.contains('cancel-button')) {
+            loadTasks(); // Carregue as tarefas para cancelar a edição
+        }
+    });
+
+    // Adiciona o evento de clique ao botão de excluir tarefa
     taskList.addEventListener('click', function (event) {
         if (event.target.tagName === 'BUTTON' && event.target.classList.contains('delete-button')) {
             const taskId = event.target.getAttribute('data-id');
@@ -44,7 +131,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const userTasks = JSON.parse(localStorage.getItem(loggedInUser)) || [];
         const taskItem = userTasks[taskId];
 
-        // Criação de elementos para editar a tarefa
+        // Cria elementos de input para editar a tarefa
         const editTaskName = document.createElement('input');
         editTaskName.value = taskItem.name;
         const editTaskDescription = document.createElement('input');
@@ -53,14 +140,14 @@ document.addEventListener('DOMContentLoaded', function () {
         editTaskDueDate.type = 'date';
         editTaskDueDate.value = taskItem.dueDate;
 
-        // Alteração do item da lista depois edição
+        // Substitue o item da lista pela edição
         const listItem = taskList.children[taskId];
         listItem.innerHTML = '';
         listItem.appendChild(editTaskName);
         listItem.appendChild(editTaskDescription);
         listItem.appendChild(editTaskDueDate);
 
-        // Adição de botões para salvar ou cancelar a edição
+        // Adiciona botões para salvar ou cancelar a edição
         const saveButton = document.createElement('button');
         saveButton.classList.add('save-button');
         saveButton.setAttribute('data-id', taskId);
@@ -80,6 +167,12 @@ document.addEventListener('DOMContentLoaded', function () {
         const editedTaskName = listItem.querySelector('input:nth-child(1)').value;
         const editedTaskDescription = listItem.querySelector('input:nth-child(2)').value;
         const editedTaskDueDate = listItem.querySelector('input:nth-child(3)').value;
+
+        const currentDate = new Date().toISOString().split('T')[0];
+        if (editedTaskDueDate < currentDate) {
+            alert('Não é possível criar tarefas com datas anteriores à data atual.');
+            return;
+        }
 
         userTasks[taskId].name = editedTaskName;
         userTasks[taskId].description = editedTaskDescription;
@@ -115,8 +208,11 @@ document.addEventListener('DOMContentLoaded', function () {
                 <strong>Nome:</strong> ${task.name}<br>
                 <strong>Descrição:</strong> ${task.description}<br>
                 <strong>Prazo:</strong> ${task.dueDate}<br>
+                <button data-id="${index}" class="complete-button">Concluir</button>
+                <div class="espace2"></div>
                 <button data-id="${index}" class="edit-button">Editar</button>
                 <button data-id="${index}" class="delete-button">Excluir</button>
+                <div class="espace"></div>
             `;
             taskList.appendChild(listItem);
         });
@@ -128,8 +224,14 @@ document.addEventListener('DOMContentLoaded', function () {
         const taskDescription = document.getElementById('task-description').value;
         const taskDueDate = document.getElementById('task-due-date').value;
 
+        const currentDate = new Date().toISOString().split('T')[0];
+        if (taskDueDate < currentDate) {
+            alert('Não é possível criar tarefas com datas anteriores à data atual.');
+            return;
+        }
+
         if (!taskName || !taskDueDate) {
-            alert('Nome da Tarefa e Prazo são campos obrigatórios!');
+            alert('Nome da Tarefa e Prazo são campos obrigatórios.');
             return;
         }
 
@@ -138,6 +240,7 @@ document.addEventListener('DOMContentLoaded', function () {
             name: taskName,
             description: taskDescription,
             dueDate: taskDueDate,
+            state: "Pendente", //Coloca o estado pendente por padrão
         };
         userTasks.push(newTask);
         localStorage.setItem(loggedInUser, JSON.stringify(userTasks));
@@ -146,5 +249,5 @@ document.addEventListener('DOMContentLoaded', function () {
         loadTasks();
     });
 
-    loadTasks(); // Carregua as tarefas iniciais
+    loadTasks(); // Carrega as tarefas iniciais
 });
